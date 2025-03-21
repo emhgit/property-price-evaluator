f6becd7f2a0487a551f75cda5d6357247638c7d6
emhgit <eharper0815@gmail.com>
2025-03-21 15:18:20 -0500

    initial commit

---
+++ src/main/java/com/starter/starter_spring_project/StarterSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.starter.starter_spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starter/starter_spring_project/StarterSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starter.starter_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
